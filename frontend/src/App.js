c1457ecb24a607ce51ad71543e7df627fb622839
WeimarAlexanderTorresHerrera <37818293+WeimarAlexanderTorresHerrera@users.noreply.github.com>
2020-03-28 13:06:01 -0400

    Initial frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+import Navigation from "./components/Navigation";
+import ChampionshipList from "./components/ChampionshipList";
+import CreateUser from "./components/CreateUser";
+import EditUser from "./components/EditUser";
 
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <Router>
+      <Navigation/>
+      <Route path="/" exact component={ChampionshipList}/>
+      <Route path="/createUser/:id" component={CreateUser}/>
+      <Route path="/editUser/:id" component={EditUser}/>
+    </Router>
   );
 }
 
