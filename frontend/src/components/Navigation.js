52d3a0d9287d32aff1b697acc0b5b359834638f8
WeimarAlexanderTorresHerrera <37818293+WeimarAlexanderTorresHerrera@users.noreply.github.com>
2020-03-29 14:45:02 -0400

    Frontend 1.1 Navigation

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Navbar, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 export default class Navigation extends Component {
     render() {
         return (
-            <div>
-                Navigation
+            <div className="containerNavbar">
+                <Navbar className="navbar justify-content-between">
+                    <div>
+                    <NavLink to="/" exact className="navLink" activeClassName="navLink-active">Home</NavLink>
+                    <NavLink to="/createUser/1" className="navLink" activeClassName="navLink-active">Create</NavLink>
+                    <NavLink to="/editUser/2" className="navLink" activeClassName="navLink-active">Edit</NavLink>
+                    </div>
+                    <Button className="button">Login</Button>
+                </Navbar>
             </div>
         )
     }
